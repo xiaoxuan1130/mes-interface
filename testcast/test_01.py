e4c16e0dae0f0e5b23be28eaba2ed0a4d01c52a6
1024046894@qq.com <1024046894@qq.com>
2021-07-02 13:58:20 +0800

    测试文件提交

---
+++ testcast/test_01.py
@@ -0,0 +1,20 @@
+import pytest
+
+def test_one():
+    print("正在执行----test_one")
+    x = "this"
+    assert 'h' in x
+
+def test_two():
+    print("正在执行----test_two")
+    x = "hello"
+    assert x
+
+def test_three():
+    print("正在执行----test_three")
+    a = "hello"
+    b = "hello world"
+    assert a in b
+
+if __name__ == "__main__":
+    pytest.main(["-s", "test_01.py"])
